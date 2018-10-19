448e04313ef75e51a7ef1cd9802c66950fcceb73
Cristtian <cristtian799@gmail.com>
2018-10-18 22:59:17 -0500

    Dos

--- js/index.js
+++ js/index.js
@@ -12,14 +12,15 @@ $(document).ready(function () {
         global.type = "text/css";
         global.id = "globalcss";
         if (true) {
+
             style.href = "" + this.value;
             global.href = "css/global.css";
             var stylesheet = document.getElementById('estilo');
             stylesheet.parentNode.removeChild(stylesheet);
 
             var stylesheet2 = document.getElementById('globalcss');
-            stylesheet2.parentNode.removeChild(stylesheet2);
-        }
+            stylesheet2.parentNode.removeChild(stylesheet2);}
+        
         /* Añadimos el estilo a la cabecera */
         document.head.appendChild(style)
         ;document.head.appendChild(global);
